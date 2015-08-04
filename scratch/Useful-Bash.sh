ec6b9d124a20fd72569cc8a0dacecb89808b08ab
Shanthanu Bhardwaj <sbhardwaj@palantir.com>
2015-08-04 00:45:07 -0700

    Added commands to create topojson JSON blob

--- scratch/Useful-Bash.sh
+++ scratch/Useful-Bash.sh
@@ -1,3 +1,4 @@
+ogr2ogr -f GeoJSON subunitsD.json IND_adm2.shp
+topojson -o in-2001.json --id-property NAME_2 --properties state=NAME_1 -- subunitsD.json
 awk -F',' '{print $3","}' List_of_Districts.txt|sort > SortedDISTS
 cat in-2001.json|  python -m json.tool |grep -i ID |sort |sed s/".*\"id\": "//g > DistrictsInJSON
-
